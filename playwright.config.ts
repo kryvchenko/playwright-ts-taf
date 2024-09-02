0ae090682d9059f47352a0ac1164050077697159
Kryvchenko <amzkryvchenko@gmail.com>
2024-09-02 17:37:14 -0400

    fix: add reporter

--- playwright.config.ts
+++ playwright.config.ts
@@ -13,11 +13,10 @@ export default defineConfig({
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : 2,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? "blob" : [["html", { open: "never" }]],
+  workers: process.env.CI ? 2 : 1,
+  reporter: [["html", { outputFolder: "playwright-report" }]],
   /* Shared settings for all the projects below */
   use: {
     screenshot: { mode: "on", omitBackground: true },
